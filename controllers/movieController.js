9ab818522016ae6f141851d77f64ddbe2d4e14da
Francesca-Lisi <francescalisi2143@gmail.com>
2025-02-09 22:00:26 +0100

    feat: add average vote to reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -21,7 +21,37 @@ const index = (req, res) => {
 //show
 const show = (req, res) => {
   const id = req.params.id;
-  res.send(`Dettagli del film con id ${id}`);
+
+  const sql = `SELECT M.*, ROUND(AVG(R.vote),1) AS average_vote
+  FROM movies M
+  LEFT JOIN reviews R ON M.id = R.movie_id
+  WHERE M.id = ?`
+
+  const sqlReviews = `SELECT *
+  FROM reviews R
+  WHERE R.movie_id = ?`
+
+  //query film
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Errore query al database' });
+    if (results.lenght === 0 || results[0].id === null) return res.status(404).json({ error: 'Film non trovato' })
+
+    //query recensioni
+    connection.query(sqlReviews, [id], (err, resultsReviews) => {
+      if (err) return res.status(500).json({ error: 'Errore query al database' });
+      console.log(resultsReviews)
+
+      const movie = results[0]
+      res.json({
+        ...movie,
+        image: req.imagePath + movie.image,
+        reviews: resultsReviews
+      })
+
+    })
+
+  })
+
 }
 
 module.exports = {
